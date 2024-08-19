f240375c5ffe63728a79e581fb76a1a7db53de10
Anvita Vadali <vvskv04@gmail.com>
2024-08-19 20:03:35 +0530

    added new feature

---
+++ converter.js
@@ -0,0 +1,4 @@
+const base_url =  "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/eur/jpy.json";
+
+const drpodowns=document.querySelector(".dropdown select");
+for(let selec of dropdowns)
